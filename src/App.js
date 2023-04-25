a4256f9105a3a8ab9d6f95ace578d1774e3003cf
aoc <michaelmoranesul@gmail.com>
2023-04-25 19:40:56 -0300

    entendendo sobre usos do link com react router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/inicio";
+import Menu from "./componentes/Menu";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>
           <Route path="/" element={<Inicio />}/>
           <Route path="/sobremim" element={<SobreMim />}/>
---
+++ src/componentes/Menu/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styles from "./Menu.module.css"
+
+export default function Menu() {
+  return (
+    <header>
+        <nav className={styles.navegacao}>
+            <Link className={styles.link} to="/">
+                Inicio
+            </Link>
+            <Link className={styles.link} to="/sobremim">
+                sobre mim
+            </Link>
+        </nav>
+    </header>
+  )
+}
