8aff8625bd4d15e2ee4ee17c4346b4bb5aa751a2
aoc <michaelmoranesul@gmail.com>
2023-04-29 09:05:06 -0300

    desenvolvendo o component cards

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SobreMim from "./paginas/SobreMim";
 import Inicio from "./paginas/inicio";
 import Menu from "./componentes/Menu";
-
+import Rodape from "componentes/Rodape";
 
 function AppRoutes() {
   return (
@@ -13,6 +13,7 @@ function AppRoutes() {
           <Route path="/sobremim" element={<SobreMim />}/>
           <Route path="*" element={<div>pagina  nao encontrada</div>}/>
       </Routes>
+      <Rodape />
     </BrowserRouter>
   )
 }
