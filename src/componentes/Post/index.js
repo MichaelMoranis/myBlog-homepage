e10bbe846d3dde7a21e4fc2de3b33e487f8f5c8f
aoc <michaelmoranesul@gmail.com>
2023-04-29 10:15:39 -0300

    adicionando minhas imagens

--- src/componentes/Post/index.js
+++ src/componentes/Post/index.js
@@ -5,7 +5,7 @@ export default function Post({ post }) {
            <div className={styles.post}>
                 <img 
                     className={styles.capa}
-                    src={`/assets/posts/${post.id}/capa.png`}
+                    src={`/assets/posts/${post.id}/capa.jpeg`}
                     alt="capa dos cards"
                 />
                 <h2 className={styles.titulo}>{post.titulo}</h2>
