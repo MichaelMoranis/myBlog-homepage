a30d9d1f6b9ee1cc3210b5cf6008959d0dadb683
aoc <michaelmoranesul@gmail.com>
2023-05-01 09:10:07 -0300

    criando estilos com o markdown, para adicionar textos nos posts, podemos usar o markdown para isso

--- src/paginas/Post/index.js
+++ src/paginas/Post/index.js
@@ -1,19 +1,26 @@
-import { useParams } from "react-router-dom"
-import posts from "json/posts.json"
+import "./Post.css"
+import { useParams } from "react-router-dom";
+import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
 
 export default function Post() {
-    const parametros = useParams();
-    const post = posts.find((post) => {
-        return post.id === Number(parametros.id)
-    })
+  const parametros = useParams();
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
 
-    return (
-       <PostModelo
-        fotoCapa={`/assets/posts/${post.id}/capa.jpeg`}
-        titulo={post.titulo}
-       >
-        {post.texto}
-       </PostModelo>
-    )
+  return (
+    <PostModelo
+      fotoCapa={`/assets/posts/${post.id}/capa.jpeg`}
+      titulo={post.titulo}
+    >
+      <div className="post-markdown-container">
+        <ReactMarkdown>
+            {post.texto}
+        </ReactMarkdown>
+      </div>
+    </PostModelo>
+  );
 }
